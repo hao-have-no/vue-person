664b6ae94e15824d3d8f9540200fe96f1267a792
fenghao <fenghao@infisa.cn>
2021-04-29 10:18:41 +0800

    生命周期进程与kepp-alive演示

---
+++ src/view/home/detail.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ContentDetail",
+        props: {
+          msg: String
+        },
+      activated() {
+        console.log('activated');
+        console.log("%c%s", "color:green","el     : " + this.$el);
+      },
+      deactivated() {
+        console.log('deactivated');
+        console.log("%c%s", "color:green","el     : " + this.$el);
+      },
+      beforeCreate: function () {
+        console.log('子组件beforeCreate');
+      },
+      created: function () {
+        console.log('子组件created ');
+      },
+      beforeMount: function () {
+        console.log('子组件beforeMount');
+      },
+      mounted: function () {
+        console.log('子组件mounted');
+      },
+      beforeUpdate: function () {
+        console.log('子组件beforeUpdate');
+      },
+      updated: function () {
+        console.log('子组件updated');
+      },
+      beforeDestroy: function () {
+        console.log('子组件beforeDestroy');
+      },
+      destroyed: function () {
+        console.log('子组件destroyed');
+      }
+    }
+</script>
+
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
--- src/view/home/list.vue
+++ src/view/home/list.vue
@@ -16,43 +16,60 @@
         <el-button type="primary" @click="onSubmit">查询</el-button>
       </el-form-item>
   </el-form>
+
+    <div id="app">
+      <p>
+        <keep-alive :exclude="excludeName">
+          <ContentDetail v-if="view" msg="Welcome to Your Vue.js App"/>
+        </keep-alive>
+      </p>
+      {{message}}
+      <button @click="change">控制子组件显示隐藏</button>&nbsp;
+      <button @click="changeexclude">控制子组件是否使用keep-alive,默认使用</button>
+    </div>
+
     <!--列表-->
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="用户名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="性别">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="年龄">
-      </el-table-column>
-    </el-table>
+    <!--<el-table-->
+      <!--:data="tableData"-->
+      <!--border-->
+      <!--style="width: 100%">-->
+      <!--<el-table-column-->
+        <!--prop="date"-->
+        <!--label="用户名"-->
+        <!--width="180">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="name"-->
+        <!--label="姓名"-->
+        <!--width="180">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="address"-->
+        <!--label="性别">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="address"-->
+        <!--label="年龄">-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
   </div>
 </template>
 
 <script>
+  import ContentDetail from './detail';
     export default {
         name: "list",
+      components:{ContentDetail},
       data() {
         return {
           formInline: {
             user: '',
             region: ''
           },
-          tableHeader:[]
+          tableHeader:[],
+          message : "Welcome wuyou",
+          view:true,
+          excludeName:''
         }
       },
       methods: {
@@ -64,7 +81,68 @@
         },
         searchSubmit(){
           console.log('search',this.formInline)
+        },
+        change(){
+          this.message = '修改过后的数据';
+          this.view = !this.view;
+        },
+        changeexclude(){
+          this.excludeName = 'contentDetail';
         }
+      },
+      beforeCreate: function () {
+        console.group('beforeCreate ');
+        console.log("%c%s", "color:red","el     : " + this.$el); //undefined
+        console.log("%c%s", "color:red","data   : " + this.$data); //undefined
+        console.log("%c%s", "color:red","message: " + this.message);//undefined
+      },
+      created: function () {
+        console.group('created ');
+        console.log("%c%s", "color:red","el     : " + this.$el); //undefined
+        console.log("%c%s", "color:green","data   : " + this.$data); //[object Object]  =>  已被初始化
+        console.log("%c%s", "color:green","message: " + this.message); //Welcome wuyou  =>  已被初始化
+      },
+      beforeMount: function () {
+        console.group('beforeMount');
+        console.log("%c%s", "color:green","el     : " + (this.$el)); //undefined
+        console.log("%c%s", "color:green","data   : " + this.$data); //已被初始化
+        console.log("%c%s", "color:green","message: " + this.message); //已被初始化
+      },
+      mounted: function () {
+        console.group('mounted');
+        console.log("%c%s", "color:green","el     : " + this.$el); //已被初始化
+        console.log(this.$el);
+        console.log("%c%s", "color:green","data   : " + this.$data); //已被初始化
+        console.log("%c%s", "color:green","message: " + this.message); //已被初始化
+      },
+      beforeUpdate: function () {
+        console.group('beforeUpdate');
+        console.log("%c%s", "color:green","el     : " + this.$el);
+        console.log(this.$el);
+        console.log("%c%s", "color:green","data   : " + this.$data);
+        let message = this.message;
+        console.log("%c%s", "color:green","message: " + message);
+      },
+      updated: function () {
+        console.group('updated');
+        console.log("%c%s", "color:green","el     : " + this.$el);
+        console.log(this.$el);
+        console.log("%c%s", "color:green","data   : " + this.$data);
+        console.log("%c%s", "color:green","message: " + this.message);
+      },
+      beforeDestroy: function () {
+        console.group('beforeDestroy');
+        console.log("%c%s", "color:red","el     : " + this.$el);
+        console.log(this.$el);
+        console.log("%c%s", "color:red","data   : " + this.$data);
+        console.log("%c%s", "color:red","message: " + this.message);
+      },
+      destroyed: function () {
+        console.group('destroyed');
+        console.log("%c%s", "color:red","el     : " + this.$el);
+        console.log(this.$el);
+        console.log("%c%s", "color:red","data   : " + this.$data);
+        console.log("%c%s", "color:red","message: " + this.message)
       }
     }
 </script>
